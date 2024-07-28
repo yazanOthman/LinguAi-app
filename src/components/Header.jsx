665be51c41ad6e32e47dfc0345d5a48feb346dda
Kito Clark <kito.clark@teamform.co>
2024-07-28 21:47:10 +1000

    added image for lingoAI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,42 @@
 import React from "react";
-import statsImg from "../imgs/stats.jpg";
+import lingoAI_chatbox from "../imgs/lingoAI_chatbox.png";
 import Button from "@mui/joy/Button";
+
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  & .content-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    position: relative;
+    z-index: 10;
+
+    &.img-container {
+      position: relative;
+    }
+
+    @media (max-width: 768px) {
+      flex-direction: column;
+      gap: 40px;
+    }
+  }
+`;
+
+const StyledImage = styled.img`
+  height: auto;
+  width: 90%;
+  box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.1);
+
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+`;
 
 const Header = () => {
   return (
     <>
-      <header id="home" className="header">
-        <div className="overlay"></div>
+      <StyledHeader id="home" className="header">
         <div className="content-wrapper">
           <div className="header-content container">
             <h1 className="header-title">
@@ -19,11 +49,18 @@ const Header = () => {
             </p>
             <Button>Join the Waitlist!</Button>
           </div>
-          <div className="image-container">
-            <img className="stat-img" src={statsImg} alt="" />
+          <div className="img-container">
+            <StyledImage
+              height="150px"
+              width={"150px"}
+              className="stat-img"
+              src={lingoAI_chatbox}
+              alt=""
+            />
           </div>
         </div>
-      </header>
+        <div className="overlay"></div>
+      </StyledHeader>
     </>
   );
 };
