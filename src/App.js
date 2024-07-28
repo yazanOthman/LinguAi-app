4007ca4555dbdd0cdc4f81ae8c22e7187de7daf3
Yazan Othman <yazanothman@Yazans-Laptop.local>
2024-07-28 18:10:39 +0800

    commented un-used pages to fix  eslint errors

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
-import HowItWorks from "./components/HowItWorks";
-import Testimonials from "./components/Testimonials";
-import FAQ from "./components/FAQ";
-import Footer from "./components/Footer";
+// import HowItWorks from "./components/HowItWorks";
+// import Testimonials from "./components/Testimonials";
+// import FAQ from "./components/FAQ";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,17 +7,19 @@ const Footer = () => {
         <div className="footer-content">
           <div className="social-media">
             <h3>Stay Connected</h3>
-            <a href="#">Facebook</a>
-            <a href="#">Twitter</a>
-            <a href="#">Instagram</a>
+            <a href="/">Facebook</a>
+            <a href="/">Twitter</a>
+            <a href="/">Instagram</a>
           </div>
           <div className="contact-info">
             <h3>Contact Us</h3>
-            <p>Email: <a href="mailto:support@finai.com">support@finai.com</a></p>
+            <p>
+              Email: <a href="mailto:support@finai.com">support@finai.com</a>
+            </p>
           </div>
           <div className="footer-links">
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms of Service</a>
+            <a href="/">Privacy Policy</a>
+            <a href="/">Terms of Service</a>
           </div>
         </div>
       </div>
