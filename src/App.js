746f071f5a52765082f680ded1118d5cc6694434
Yazan Othman <yazanothman@Yazans-Laptop.local>
2024-07-28 20:00:50 +0800

    created modal component and integrated it with email js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
+import { Toaster } from "react-hot-toast";
+
 // import HowItWorks from "./components/HowItWorks";
 // import Testimonials from "./components/Testimonials";
 // import FAQ from "./components/FAQ";
@@ -11,6 +13,7 @@ import Pricings from "./components/Pricings";
 function App() {
   return (
     <div className="App">
+      <Toaster />
       <Navbar />
       <Header />
       <main>
